a1adac8d43b38db012fc793149836e998c3c5def
Aaron Pérez Alfaro <a.perez.1@estudiantec.cr>
2025-01-26 15:37:31 -0600

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+vvi adj;
+set<int> cities;
+
+vector<bool> visited;
+vi tin, low;
+int timer;
+
+void IS_CUTPOINT(int v){
+    cities.insert(v);
+}
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    int children=0;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] >= tin[v] && p!=-1)
+                IS_CUTPOINT(v);
+            ++children;
+        }
+    }
+    if(p == -1 && children > 1)
+        IS_CUTPOINT(v);
+}
+
+void find_cutpoints(int n) {
+    timer = 0;
+    visited.assign(n, false);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs (i);
+    }
+}
+
+int main() {
+    int n,q; 
+    cin >> n >> q;
+
+    vi a(n); 
+    for(auto &i : a) cin >> i;
+    
+    adj.assign(n, vi());
+    
+    for (int i = 0; i < q; i++){
+        int u,v; cin >> u >> v;
+        u--; v--;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    find_cutpoints(n);
+
+    for (auto &i : cities){
+        cout << i << ' ';
+    }
+
+
+    return 0;
+}
